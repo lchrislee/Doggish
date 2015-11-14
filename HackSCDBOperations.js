b49a6c6b4dbffaea8f378fbca445eccc17bd23e5
Ryan Zhou <ryanzhou7@gmail.com>
2015-11-14 14:06:52 -0800

    Added sample json operations to DB

---
+++ HackSCDBOperations.js
@@ -0,0 +1,48 @@
+//USERS
+case 'create':
+dynamo.putItem(event, context.succeed('create') );
+{
+  "operation": "create",
+  "TableName": "Users",
+  "Item": {
+    "ID":"1",
+    "name": "name2",
+    "Email": "email1",
+    "points":"0",
+    "level":"1",
+    "zipCode":"90007",
+    "imageUrl":"imageUrl1"
+  }
+}
+
+case 'read':
+{
+
+	"operation": "read",
+	"TableName": "Users",
+	"Key":{
+		"ID":"1"
+	},
+	"AttributesToGet": [
+      "name"
+    ]
+}
+
+case 'update'
+{
+	"operation": "update",
+	"TableName": "Users",
+    "Key": {
+        "ID": "1"
+    },
+    "ExpressionAttributeNames":{ "#n": "name" },
+    "UpdateExpression": "SET #n = :val",
+    "ExpressionAttributeValues": { 
+        ":val": "new name"
+    },
+    "ReturnValues": "UPDATED_NEW"
+    //don't need to return
+}
+
+
+
