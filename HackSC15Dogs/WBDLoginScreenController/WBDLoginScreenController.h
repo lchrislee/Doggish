bfb60edca8ce8476fb99f23cd95fcae3ed243384
Chris Lee <chrisl@usc.edu>
2015-11-15 00:06:19 -0800

    Added extra screen.

---
+++ HackSC15Dogs/WBDLoginScreenController/WBDLoginScreenController.h
@@ -0,0 +1,13 @@
+//
+//  WBDLoginScreenController.h
+//  HackSC15Dogs
+//
+//  Created by abc on 11/14/15.
+//  Copyright © 2015 Wannabedev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WBDLoginScreenController : UIViewController
+
+@end
---
+++ HackSC15Dogs/WBDLoginScreenController/WBDLoginScreenController.m
@@ -0,0 +1,29 @@
+//
+//  WBDLoginScreenController.m
+//  HackSC15Dogs
+//
+//  Created by abc on 11/14/15.
+//  Copyright © 2015 Wannabedev. All rights reserved.
+//
+
+#import "WBDLoginScreenController.h"
+
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+
+@interface WBDLoginScreenController ()
+@property (strong, nonatomic) FBSDKLoginButton *loginButton;
+@end
+
+@implementation WBDLoginScreenController
+
+- (void) viewDidLoad{
+    self.loginButton = [[FBSDKLoginButton alloc] init];
+    self.loginButton.readPermissions = @[@"user_friends",@"public_profile"];
+    
+    self.loginButton.center = self.view.center;
+    
+    [self.view addSubview:self.loginButton];
+}
+
+@end
